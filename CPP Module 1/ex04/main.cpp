c13d738cf16b227a56da5c2d954f0071b5733cc9
Alperen Rüzgar Erbosnalı <97199520+Vencoheria@users.noreply.github.com>
2023-05-29 23:13:41 +0300

    CPP 01 Ex04 done n done babe

--- CPP Module 1/ex04/main.cpp
+++ CPP Module 1/ex04/main.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
 #include <fstream>
-//testing
-
 
 int main(int ac, char **av) {
 	if (ac == 4)
 	{
 		std::string theAdress = av[1];
-		std::string theAdress = av[1] + ;
 		std::string oldWord = av[2];
 		std::string newWord = av[3];
+		std::string wholeLine;
 		std::string holder;
 		std::ifstream inFile(theAdress);
-		std::ofstream outFile();
+		size_t where;
 
 		if (inFile.fail()) {
 			std::cerr << "Failed to open the file." << std::endl;
 			exit (1);
 		}
-		
-		while (inFile >> holder) {
-			std::cout << holder << std::endl;
+
+		std::ofstream outFile(theAdress + ".replace", std::ios::trunc);
+		if (outFile.fail()) {
+			std::cerr << "Failed to open the file." << std::endl;
+			exit (2);
 		}
+		while (std::getline(inFile, wholeLine)){
+			where = 0;
+			holder = wholeLine;
+			while ((where = wholeLine.find(oldWord, where)) != std::string::npos)
+			{
+				holder.erase(where, oldWord.length());
+				holder.insert(where, newWord);
+				where += newWord.length();
+			}
+		outFile << holder << std::endl;
+	}
 	}
 	else
-		std::cout << "Insufficent Arguments. Please follow the template : ./replace [filename/fileadress] [the word to be replaced] [the word to replace]" << std::endl;
+		std::cout << "Insufficent/Wrong Arguments. Please follow the template : ./replace [filename/fileadress] [the word to be replaced] [the word to replace]" << std::endl;
 	return 0;
 }

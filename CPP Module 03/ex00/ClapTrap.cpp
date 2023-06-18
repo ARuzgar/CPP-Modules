2e5ad13aeb9b0839962d146419e27f4938ecb4a2
Alperen Rüzgar Erbosnalı <97199520+Vencoheria@users.noreply.github.com>
2023-06-18 23:12:46 +0300

    Welcome to CPP 03

---
+++ CPP Module 03/ex00/ClapTrap.cpp
@@ -0,0 +1,51 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void)
+{
+	
+}
+
+ClapTrap::ClapTrap(std::string name)
+{
+	
+}
+
+ClapTrap::ClapTrap(ClapTrap const &copy)
+{
+	
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	
+}
+
+ClapTrap &	ClapTrap::operator=(ClapTrap const &clapTrap)
+{
+	
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	
+}
+
+std::string	ClapTrap::getName(void)
+{
+	
+}
+
+int	ClapTrap::getAttackDamage(void)
+{
+	
+}
---
+++ CPP Module 03/ex00/ClapTrap.hpp
@@ -0,0 +1,30 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <sstream>
+
+class ClapTrap
+{
+	private:
+
+	std::string name;
+	int			hitPoints;
+	int			energyPoints;
+	int			attackDamage;
+
+	public:
+
+		ClapTrap(void);
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap const &copy);
+		ClapTrap &operator=(ClapTrap const &clapTrap);
+		~ClapTrap(void);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		std::string getName(void);
+		int getAttackDamage(void);
+};
+
+#endif

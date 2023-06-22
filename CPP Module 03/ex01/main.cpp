ba81b1136760e855ed2d898e62fea886162191ee
Alperen Rüzgar Erbosnalı <97199520+Vencoheria@users.noreply.github.com>
2023-06-22 17:29:59 +0300

    auto commit

--- CPP Module 03/ex01/main.cpp
+++ CPP Module 03/ex01/main.cpp
@@ -1,15 +1,13 @@
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
-int main(void)
-{
-	ClapTrap *clapTrapHorde = new ClapTrap[3];
-	ClapTrap target("Daddy Melih");
-	for (int i = 0; i < 3; i++)
-	{
-		clapTrapHorde[i].attack(target.getName());
-		target.takeDamage(clapTrapHorde[i].getAttackDamage());
-	}
-	delete [] clapTrapHorde;
+int main(void){
+	ClapTrap target("Melih");
 	target.beRepaired(3);
+	ScavTrap s;
+	s.attack("Melih");
+	s.beRepaired(10);
+	s.guardGate();
+	s.takeDamage(15);
 	return (0);
 }

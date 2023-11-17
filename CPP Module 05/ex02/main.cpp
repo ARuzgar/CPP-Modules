3e276639cc0c4a1221a0dfb565b6465641a6b27e
Alperen Rüzgar Erbosnalı <97199520+ARuzgar@users.noreply.github.com>
2023-11-17 23:38:25 +0300

    tester

--- CPP Module 05/ex02/main.cpp
+++ CPP Module 05/ex02/main.cpp
@@ -1,11 +1,15 @@
 #include "Bureaucrat.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main(void)
 {
 	Bureaucrat bureaucrat("Bureaucrat", 1);
 	Bureaucrat bureaucrat2("Bureaucrat2", 150);
 	AForm *form;
+
+	std::cout << std::endl << "PresidentialPardonForm" << std::endl;
 
 	form = new PresidentialPardonForm("Jack Sparrow");
 	std::cout << *form << std::endl;
@@ -16,6 +20,25 @@ int main(void)
 	bureaucrat2.signForm(*form);
 	delete form;
 
+	std::cout << std::endl << "RobotomyRequestForm" << std::endl;
+
+	form = new RobotomyRequestForm("Bender");
+	std::cout << *form << std::endl;
+	bureaucrat.signForm(*form);
+	bureaucrat.executeForm(*form);
+	bureaucrat2.executeForm(*form);
+	bureaucrat2.signForm(*form);
+	delete form;
+
+	std::cout << std::endl << "ShrubberyCreationForm" << std::endl;
+
+	form = new ShrubberyCreationForm("Home");
+	std::cout << *form << std::endl;
+	bureaucrat.signForm(*form);
+	bureaucrat.executeForm(*form);
+	bureaucrat2.executeForm(*form);
+	bureaucrat2.signForm(*form);
+	delete form;
 	
 	return (0);
 

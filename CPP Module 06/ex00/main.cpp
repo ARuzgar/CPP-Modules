8abb98c18ef36c7117100dd7c47aaf03bf6821d7
Alperen Rüzgar Erbosnalı <97199520+ARuzgar@users.noreply.github.com>
2023-11-19 00:32:22 +0300

    Felt a bit fancy and wanted to try out char and int as well, it looks like there is a end for Infinity and NaN as an integer. Weird, huh.

--- CPP Module 06/ex00/main.cpp
+++ CPP Module 06/ex00/main.cpp
@@ -5,6 +5,26 @@
 
 int main(int ac, char **av)
 {
+    int positive_infinity_int = INFINITY;
+    int negative_infinity_int = -INFINITY;
+    int nan_value_int = NAN;
+
+    std::cout << "Positive Infinity Int: " << positive_infinity_int << std::endl;
+    std::cout << "Negative Infinity Int: " << negative_infinity_int << std::endl;
+    std::cout << "NaN Int: " << nan_value_int << std::endl;
+
+    std::cout << std::endl;
+
+    char positive_infinity_char = INFINITY;
+    char negative_infinity_char = -INFINITY;
+    char nan_value_char = NAN;
+
+    std::cout << "Positive Infinity Char: " << positive_infinity_char << std::endl;
+    std::cout << "Negative Infinity Char: " << negative_infinity_char << std::endl;
+    std::cout << "NaN Char: " << nan_value_char << std::endl;
+
+    std::cout << std::endl;
+    
     float positive_infinity = INFINITY;
     float negative_infinity = -INFINITY;
     float nan_value = NAN;
@@ -24,9 +44,6 @@ int main(int ac, char **av)
     std::cout << "NaN Double: " << nan_value_double << std::endl;
 
     std::cout << std::endl;
-
-
-
 
     return (0);
 }

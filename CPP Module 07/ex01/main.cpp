b88a6467500da1fdb07f6de0b1b205500035fee0
Alperen Erbosnali <aerbosna@k1m43s03.42istanbul.com.tr>
2023-12-10 19:21:35 +0300

    Yoinked the tester from Melih for ex02, because why not?, fixed 01(Didn't even know it was erroneous, figured it out the way while trying to make my sub-funcs generic as well), added a resulation operator to swap in 00's main. Gonna take it to a ride now

--- CPP Module 07/ex01/main.cpp
+++ CPP Module 07/ex01/main.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
-#include <unistd.h>
+
 #include "iter.hpp"
 
-void print(int &a) {
-	std::cout << a << std::endl;
+template <typename TA >
+void print(TA &a) {
+	std::cout << reset << a << magenta << std::endl;
 }
 
-void cast(int &a) {
+template <typename TA >
+void cast(TA &a) {
 	srand(time(0));
-	a = rand() % 9 + 1;
-	std::cout << "rand loading..." << std::endl;
+	a = rand() % 9;
+	std::cout << schwarz << "rand loading..." << std::endl;
 	usleep(696969);
 }
 
-void add(int &a) {
+template <typename TA >
+void addOne(TA &a) {
 	a += 1;
 }
 
-void printc(char &a) {
-	std::cout << a << std::endl;
-}
-
 int main( void ) {
+	std::cout << scrclean << std::endl;
 	int array[5] = {512, 213, 134, 1287, 123};
-	std:: cout << "Array print with iter:" << std::endl;
-	iter(array, 5, print);
-	iter(array, 5, cast);
-	std:: cout << "Array print with iter after rand cast with iter :" << std::endl;
-	iter(array, 5, print);
-	iter(array, 5, add);
-	std:: cout << "Array print with iter after add with iter :" << std::endl;
-	iter(array, 5, print);
-
-	char array2[5] = {'a', 'b', 'c', 'd', 'e'};
-	std:: cout << "char array print with iter:" << std::endl;
-	iter(array2, 5, printc);
-
+	 std::cout << magenta << "Array print with iter:" << std::endl << std::endl;
+    iter(array, 5, print<int>);
+	std::cout << std::endl;
+    iter(array, 5, cast<int>);
+    std::cout << std::endl << magenta << "Array print with iter after rand cast with iter :" << std::endl << std::endl ;
+    iter(array, 5, print<int>);
+	std::cout << std::endl;
+    iter(array, 5, addOne<int>);
+    std::cout << "Array print with iter after addOne with iter :" << std::endl << std::endl;
+    iter(array, 5, print<int>);
+	std::cout << std::endl;
+    char array2[5] = {'a', 'b', 'c', 'd', 'e'};
+    std::cout << "Char array print with iter:" << std::endl << std::endl;
+    iter(array2, 5, print<char>);
 
 	return 0;
 }
--- CPP Module 07/ex02/main.cpp
+++ CPP Module 07/ex02/main.cpp
@@ -1,55 +1,38 @@
-#include <iostream>
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+int main(void)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	Array<int> a(5);
+	Array<char> b(5);
+	Array<float> c(5);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+	for (unsigned int i = 0; i < a.size(); i++)
+		a[i] = i;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
+	for (unsigned int i = 0; i < b.size(); i++)
+		b[i] = 'a' + i;
+	
+	for (unsigned int i = 0; i < c.size(); i++)
+		c[i] = i + 0.5;
+	
+	for (unsigned int i = 0; i < a.size(); i++)
+		std::cout << a[i] << std::endl;
+	
+	for (unsigned int i = 0; i < b.size(); i++)
+		std::cout << b[i] << std::endl;
 
-    delete [] mirror;
-	system("leaks arrrrr");
-    return 0;
+	for (unsigned int i = 0; i < c.size(); i++)	
+		std::cout << c[i] << std::endl;
+	
+	try {
+		std::cout << a[10] << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << a[-1] << std::endl;
+	} catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
